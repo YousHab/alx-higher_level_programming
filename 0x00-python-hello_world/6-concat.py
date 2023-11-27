e2a0908fde3357e94092a8d8ce8c5463985c8da1
Youssef Haboul <haboulyousse14@gmail.com>
2023-11-27 23:06:57 +0100

    m

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 += " " + str2 + "\n"
+str1 += "Welcome" + " " + str2 + "\n"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"Welcome to {str1}!")
