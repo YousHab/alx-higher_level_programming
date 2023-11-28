129be9ccef411c8de2847d1a5ef8a0fadbbc710c
Youssef Haboul <haboulyousse14@gmail.com>
2023-11-28 20:07:06 +0100

    m

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/user/bin/python3
+def fizzbuzz():
+    for i in range(0, 101):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                print("FizzBuzz", end=" ")
+            else:
+                print("Fizz", end=" ")
+        elif i % 5 == 0:
+            if i % 3 == 0:
+                print("FizzBuzz", end=" ")
+            else:
+                print("Buzz", end=" ")
+        else:
+            print("{}".format(i), end=" ")
+    print("$")
