df00960ee732ddccbdae91535e8c7ab5ca42fbaa
Youssef Haboul <haboulyousse14@gmail.com>
2023-11-30 16:13:34 +0100

    m

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv
     n = len(argv) - 1
     print("{} arguments".format(n), end="")
-    if n = 0:
+    if n == 0:
         print(".")
     else:
         print(":")
-        for i in range(1, len(argv) + 1):
+        for i in range(1, n + 1):
             print("{}: {}".format(i, argv[i]))
