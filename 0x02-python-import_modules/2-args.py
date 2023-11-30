9d797813c1712e53caea428c1770d347171746e9
Youssef Haboul <haboulyousse14@gmail.com>
2023-11-30 16:10:54 +0100

    m

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    from sys import argv
+    n = len(argv) - 1
+    print("{} arguments".format(n), end="")
+    if n = 0:
+        print(".")
+    else:
+        print(":")
+        for i in range(1, len(argv) + 1):
+            print("{}: {}".format(i, argv[i]))
