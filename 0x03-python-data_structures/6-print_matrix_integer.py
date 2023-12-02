13fe2a5dbce7332adcfa7324b3c898afd4f2b5eb
Youssef Haboul <haboulyousse14@gmail.com>
2023-12-02 14:28:55 +0100

    m

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     n = len(matrix) - 1
-    for i in range(0, n):
+    for i in range(n + 1):
         m = len(matrix[i]) - 1
-        for j in range(0, m):
+        for j in range(m + 1):
             if j == m:
                 print("{}".format(matrix[i][j]), end="$\n")
             else:
