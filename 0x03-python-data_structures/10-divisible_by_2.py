cb6398199a8c3d9fc16689fb47321a127752693a
Youssef Haboul <haboulyousse14@gmail.com>
2023-12-04 16:01:25 +0100

    m

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    div_by_2 = []
+    for elt in my_list:
+        if elt % 2 == 0:
+            div_by_2.append("True")
+        else:
+            div_by_2.append("False")
+    return div_by_2
