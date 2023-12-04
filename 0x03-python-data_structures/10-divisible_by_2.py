aa800fe942570faa6bc54ebbead8ea0ef006dd58
Youssef Haboul <haboulyousse14@gmail.com>
2023-12-04 16:03:07 +0100

    m

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     div_by_2 = []
     for elt in my_list:
-        if elt % 2 == 0:
-            div_by_2.append("True")
+        if (elt % 2) == 0:
+            div_by_2.append(True)
         else:
-            div_by_2.append("False")
+            div_by_2.append(False)
     return div_by_2
