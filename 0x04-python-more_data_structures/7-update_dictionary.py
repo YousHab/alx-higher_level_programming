324d8cd2ac9d85327ad2f865a66ece3b4434623f
Youssef Haboul <haboulyousse14@gmail.com>
2023-12-07 00:22:30 +0100

    m

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    flag = 0
-    for k, v in a_dictionary.items():
-        if k == key:
-            v = value
-            flag = 1
-        print("{}: {}".format(k, v))
-    if flag == 0:
-        a_dictionary[key] = value
-        print("{}: {}".format(key, value))
+    a_dictionary[key] = value
+    return a_dictionary
