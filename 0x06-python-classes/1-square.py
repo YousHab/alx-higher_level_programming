b8cd2b29347ab87da138b160948435ac73e77341
Youssef Haboul <haboulyousse14@gmail.com>
2023-12-29 14:47:55 +0100

    m

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define the Square class"""
+
+
+class Square:
+    """Class definition"""
+
+    def __init__(self, size=0):
+        """Constructor
+        Args:
+            size : the size of the square
+        """
+        self.size = size
