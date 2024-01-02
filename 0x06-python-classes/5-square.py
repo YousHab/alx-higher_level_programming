bfef958b41b02c67a4109728e166fc239ce50576
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-02 01:59:13 +0100

    m

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Define the Square class"""
+
+
+class Square:
+    """ Square class
+    Attributes:
+        size: the size of the square
+    """
+
+    def __init__(self, size=0):
+        """Creates new instances of square
+
+        Args:
+            size: the size of square
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of a square.
+
+        Returns: the area of square
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * (self.__size))
+
+    @property
+    def size(self):
+        """Returns the size of a square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter for size.
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
