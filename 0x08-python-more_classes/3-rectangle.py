f8b963ba392a143eaeb4cdebf495f87adf2367f3
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-04 15:40:32 +0100

    m

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" RECTANGLE TASK3
+""" RECTANGLE TASK1
 """
 
 
@@ -7,6 +7,12 @@ class Rectangle:
     """ A class representing a rectangle
     """
     def __init__(self, width=0, height=0):
+        """ attribute assigment here engages setters defined below
+
+        Attributes:
+            width: the horizontal dimension of rectangle
+            height: the vertical dimension of rectangle
+        """
         self.__width = width
         self.__height = height
 
@@ -27,7 +33,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__height + self.__width)
-
+    
     def __str__(self):
         """ print the rectangle with the character #
 
@@ -59,7 +65,7 @@ class Rectangle:
             TypeError: If `value` is not an int.
             ValueError: If `value` is less than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -88,7 +94,7 @@ class Rectangle:
             TypeError: If `value` is not an int.
             ValueError: If `value` is less than 0.
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
