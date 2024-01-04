273bf487fb256ee024933547f836dcf440254d69
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-05 00:42:47 +0100

    m

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -101,4 +101,4 @@ class Rectangle:
         """
         if self.__height == 0 or self.__width == 0:
             return ""
-        return ("#" * self.__width + '\n') * self.__height
+        return "\n".join(['#' * self.__width for _ in range(self.__height)])
