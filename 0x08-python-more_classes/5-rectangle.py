cdf23a4c0b6ca807b300a5fa5d5f463701157769
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-05 01:01:52 +0100

    m

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -102,7 +102,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         return "\n".join(['#' * self.__width for _ in range(self.__height)])
-    
+
     def __repr__(self):
         """Allows use of eval().
 
