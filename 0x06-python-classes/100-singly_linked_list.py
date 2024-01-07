563705a79d2bc951a43043640bf3b2e854e05958
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-07 18:13:39 +0100

    m

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,14 +2,14 @@
 
 class Node:
     def __init__(self, data, next_node=None):
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
         return self.__data
     
-    @property.setter
+    @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
@@ -18,5 +18,23 @@ class Node:
     
     @property
     def next_node(self):
+        return self.__next_node
+    
+    @next_node.setter
+    def next_node(self, value):
+        if value == None or not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+    
+    def __str__(self):
         
-        
+        string1 = ""
+        current_node = self.__head
+        while current_node and isinstance(current_node, Node):
+            string1 += str(current_node.data) + '\n'
+            current_node = current_node.next_node
+        return string1
