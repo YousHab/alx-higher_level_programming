48d55b77f6391abb71c63d68e205abef8ced36d4
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-10 17:40:24 +0100

    m

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """
 ===================================
-module with class BaseGeometry
+module with classes BaseGeometry and Rectangle
 ===================================
 """
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
+        """ Calculate the area of the rectangle
+        """
         return self.__width * self.__height
-
+    
     def __str__(self):
-        """__str__ method for return the next string"""
+        """__str__ method for return the next stringù
+        """
 
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
