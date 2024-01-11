cfb4aa15b922d164dadfc98dda707548c59f62e7
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-11 21:39:49 +0100

    m

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""
+Module with the function write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    function that writes a string to a text file.
+
+    Args:
+        filename: the name of the file.
+        text: the text to write.
+
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as my_file:
+        my_file.write(text)
+        return len(text)
