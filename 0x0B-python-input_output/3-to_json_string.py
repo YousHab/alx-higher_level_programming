6cb2f5dcd825fa2a6382c9a7391696de286d47e7
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-11 21:58:47 +0100

    m

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""
+Module with the fucntion append_write
+"""
+
+def append_write(filename="", text=""):
+    """
+    This fucniton appends a string at the end of
+    a text file (UTF8)
+
+    Args:
+        filename: the file's name
+        text: the text to add.
+
+    Returns:
+        The number of appended characters
+    """
+    with open(filename, "a", encoding="utf-8") as my_file:
+        my_file.write(text)
+        return len(text)
