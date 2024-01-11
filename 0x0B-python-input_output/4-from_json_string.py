7276ab0b560f1b8b5645ab25ea01c97938e1fe3e
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-11 22:34:41 +0100

    m

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""
+Module with the function from_json_string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    function that returns an object (Python data structure)
+    represented by a JSON string.
+
+    Args:
+        my_str: the string to be transformed
+
+    Returns:
+        An object (Python data structure)
+        represented by a JSON string
+    """
+
+    my_data = json.loads(my_str)
+    return my_data
