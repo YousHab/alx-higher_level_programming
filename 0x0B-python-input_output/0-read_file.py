45a5b7c53dc63e7eff4fd1b7f8f80b48d9255c03
Youssef Haboul <haboulyousse14@gmail.com>
2024-01-11 21:12:35 +0100

    m

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,4 +15,5 @@ def read_file(filename=""):
 
     """
     with open("filename", 'r', encoding="utf-8") as my_file:
-        print(my_file.read())
+        content = my_file.read()
+        print(content)
