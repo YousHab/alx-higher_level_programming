df0ffd1b89529a4daeb9f0679b6ac0c2e5b9fed9
Youssef Haboul <haboulyousse14@gmail.com>
2024-03-22 22:57:20 +0000

    m

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Write a script that prints the first argument passed to it
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
