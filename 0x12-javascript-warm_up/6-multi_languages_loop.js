a7eff4075c2c5af0cb35a56f41e95423898530ff
Youssef Haboul <haboulyousse14@gmail.com>
2024-03-23 00:04:06 +0000

    m

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that prints 3 lines by using an array of string and a loop.
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (elt of myArray){
+  console.log(elt);
+}
