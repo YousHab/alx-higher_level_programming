7b23ebacab5687f53a58b78a43998b5f00ecdaf9
Youssef Haboul <haboulyousse14@gmail.com>
2024-03-23 00:18:28 +0000

    m

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Script that prints x times "C is fun"
 
-if (isNaN(process.argv[2])){
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurences');
 } else {
-  let i=0;
-  while (i < parseInt(process.argv[2])){
+  let i = 0;
+  while (i < parseInt(process.argv[2])) {
     console.log('C is fun');
     i++;
   }
