07c1a42e4cf23ce775b3f7c666ef874492f85cbd
Youssef Haboul <haboulyousse14@gmail.com>
2024-04-07 22:40:09 +0000

    m

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,10 +2,11 @@
 const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
-    if (size > 0) {
-      super(size, size);
-    }
+    super(size, size);
   }
 }
 
 module.exports = Square;
+
+const square = new Square(-4);
+console.log(square);
