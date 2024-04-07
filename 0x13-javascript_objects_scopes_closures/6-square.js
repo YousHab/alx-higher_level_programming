6cbdf9e5d07dbba2b87af26659d25e2b831d450d
Youssef Haboul <haboulyousse14@gmail.com>
2024-04-07 22:51:31 +0000

    m

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Square extends require('./5-square') {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
