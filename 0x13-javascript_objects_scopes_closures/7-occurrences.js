07bed9932733b3c19a113d66d3fda20b24360075
Youssef Haboul <haboulyousse14@gmail.com>
2024-04-07 23:07:33 +0000

    m

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-function nbOccurences (list, searchElement) {
-  return list.reduce((count, elt) => elt === searchElement ? count++ : count, 0);
-}
-module.exports = nbOccurences;
+exports.nbOccurences = function (list, searchElement) {
+    return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+};
