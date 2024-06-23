956aa2722717e27e4d1e4470221cb2aad178af0c
haboul youssef <haboulyoussef14@gmail.com>
2024-06-23 11:19:25 -0700

    m

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name FROM cities\
-                INNER JOIN states ON cities.states_id = states.id\
+                INNER JOIN states ON cities.state_id = states.id\
                 WHERE states.name = %s", [argv[4]])
     rows = cur.fetchall()
     
