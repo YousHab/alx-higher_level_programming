55003b834c290714773d5d1e746ba4c81c3e3c44
haboul youssef <haboulyoussef14@gmail.com>
2024-06-23 10:18:13 -0700

    m

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
 """
-
+a script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
 """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = %s"
+    cur.execute(query, [argv[4]])
+
+    rows = cur.fetchall()
+    for elts in rows:
+        print(elts)
+
+    cur.close()
+    db.close()
